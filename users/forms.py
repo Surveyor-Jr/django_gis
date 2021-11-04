a20e8cec69cad2f3de5386bacf260f498f78cc61
Surveyor-Jr <matingonk@gmail.com>
2021-11-04 22:05:04 +0200

    added Login views but they do not really work for now

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,11 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+
+# Customised Login form 
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    password = forms.PasswordInput(attrs={'class': 'form-control'})
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
