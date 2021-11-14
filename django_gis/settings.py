250806c63111beb9f8a4246ba4e5b11cfc7ae890
Surveyor-Jr <matingonk@gmail.com>
2021-11-14 23:26:28 +0200

    changed settings

--- django_gis/settings.py
+++ django_gis/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-4al9x6x!tbq_(nfuz40jcw%&q92&m(069tinr0n!1br@xp!a7k'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
